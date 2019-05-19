f841bac96f9e41b0f59a9e6694e7ce02f94f2ca5
Joel Savitz <joelsavitz@gmail.com>
2019-05-19 17:51:30 -0400

    add documentation to node.h

--- node.h
+++ node.h
@@ -23,6 +23,9 @@ typedef union metadata {
 	long long	data ;
 } metadata_t ;
 
+/* these are the operations of must expect to be able to perform on
+ * a nodes data
+ */
 typedef struct node_data_ops {
 	void 		*(*get_bytes)(struct node *) ;
 	size_t		(*get_length)(struct node *) ;
@@ -32,8 +35,11 @@ typedef struct node_data_ops {
 	int 		(*compare_data)(void * first, void * second) ;
 } data_ops_t ;
 
-/* Creation functions will be specified for each node specializaiton,
- * but they all must know how to kill themselves and reproduce
+/* this is the core building block of the database
+ *
+ * creation functions will be specified for each node specializaiton,
+ * but they all must know how to and reproduce. The used to need to
+ * know how to kill themselves, but the do not anymore.
  */
 struct node {
 	udid_t 		udid ;
@@ -51,24 +57,60 @@ struct node {
 	struct node     *(*create_child)(void * args) ;
 } ;
 
+/* free a node and all associated memory */
 void node_kill(struct node ** node) ;
 
+/* return true if a node has more than zero children */
 bool node_has_children(struct node * node) ;
+
+/* return true if a node has a parent node (vs created by client call to create()) */
 bool node_has_parent(struct node * node) ;
 
+/* a data structure validity check that returns true when the node's internal count
+ * of all child nodes is corroberated by behavior observed by the linked list structure
+ * used to contain the child nodes
+ */
 bool node_verify_child_count(struct node * parent) ;
+
+/* a data structure validity check that reutrns true when our three seperate counts
+ * of the number of child nodes are equivelent, the two calculated by iteration
+ * and the the internal count of the parent node
+ */
 bool node_verify_sibling_integrity(struct node * parent) ;
 
+/* create a node and insert it into the child list of the parent
+ * return true unless something went wrong
+ */
 bool node_create_child(struct node * parent, void * args) ;
+
+/* insert a node into the child list of the parent
+ * return true unless something went wrong
+ */
 bool node_push_child(struct node * parent, struct node * child) ;
 
+/* access the child list of a parent node as one would an array
+ * and return a pointer to the requested node, or NULL if something went wrong
+ */
 struct node * node_get_child_by_index(struct node * parent, size_t child_index) ;
+
+/* return the length of the parent's child node list */
 size_t node_get_child_count(struct node * parent) ;
 
+/* iterate through a parent's child node list, killing all after a certain index
+ * until either all children have been killed or the body count limit has been
+ * reached
+ */
 bool node_kill_child(struct node * parent, size_t child_index, size_t body_count_limit) ;
+
+/* free all data associated with a parent's child node list and set all pointers to
+ * that list to NULL
+ */
 bool node_kill_all_children(struct node * parent) ;
 
-void node_print_child_linkage(struct node * node, FILE * file) ;
+/* display the hex addresses of a node, it's brother, and it's sister */
 void node_print_linkage(struct node * node, FILE * file) ;
+
+/* call print_linkage() on all children of a node */
+void node_print_child_linkage(struct node * node, FILE * file) ;
 
 #endif /* NODE_H */
